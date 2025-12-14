f8da2fc1d4ef2338bc7c1dc0ea745d2b5d346a9f
1vrk <morfn669@gmail.com>
2025-12-14 20:56:13 +0300

    -

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -28,6 +28,6 @@ def get_current_admin_user(
     if not current_user.is_admin:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="The user doesn't have enough privileges"
+            detail="У вас нет прав"
         )
     return current_user
